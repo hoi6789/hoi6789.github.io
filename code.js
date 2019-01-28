770b57f5fcbcca9a6eb23f800591f96de93188a7
hoi6789 <poo_hoi@hotmail.com>
2019-01-28 18:08:09 -0500

    Update code.js

--- code.js
+++ code.js
@@ -152,7 +152,7 @@ function navUpdate() {
 }
 
 function slash(id, attack, accuracy, level, target, basepower) {
-	activeDataHP[target] -= function damage(id, attack, accuracy, level, target, basepower, "none", "none", 0, "none", 0, 0, "physical", 1)
+	activeDataHP[target] -= function damage(id, attack, accuracy, level, target, basepower, "none", "none", 0, 0, "none", 0, "physical", 1);
 }
 function statusSelf(player, status, length, animation) {
 	for(i = 0; i < length; i++) {
@@ -160,7 +160,7 @@ function statusSelf(player, status, length, animation) {
 	}
 }
 function damage(id, attack, accuracy, level, target, basepower, element, status, chance, count, debuff, amp, type, acc) {
-	var hurt = Math.ceil(attack * Math.pow(scalemult, levelPassive[level]) * basepower * 2.5) 
+	var hurt = Math.ceil(attack * Math.pow(scalemult, levelPassive[level]) * basepower * 2.5);
 	switch(activeDataID[target]) {
 		case 0: def = 0;
 		break;
@@ -178,4 +178,5 @@ function damage(id, attack, accuracy, level, target, basepower, element, status,
 	if(dodgecheck > dodge) {
 		hurt = 0;
 	}
+	return 4;
 }
