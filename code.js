4c5cfb0bdebd0fa5e4a8fbf99cf9ced811bf0171
hoi6789 <poo_hoi@hotmail.com>
2019-03-12 22:18:46 -0400

    Update code.js

--- code.js
+++ code.js
@@ -258,11 +258,11 @@ function damagefremb(attack, accuracy, user, target, basepower, element, percent
 	//determining evade
 	var evade = levelscalefoe(tgt.evd, levelHostile[target]);
 	//checking for dodge
-	var dodge = (accuracy * acc * (1 + allyBuffs[user][4])) / (evade * (1 + foeBuffs[tgt][5]));
-	var dodgecheck = Math.random();
-	if(dodgecheck > dodge) {
-		return 0;
-	}
+	//var dodge = (accuracy * acc * (1 + allyBuffs[user][4])) / (evade * (1 + foeBuffs[tgt][5]));
+	//var dodgecheck = Math.random();
+	//if(dodgecheck > dodge) {
+	//	return 0;
+	//}
 	//determines elemental resistances
 	//hurt * tgt.
 	
