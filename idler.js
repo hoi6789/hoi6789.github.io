4bb6572350f8ef763c2cb6499bff816bdab1dfe2
hoi6789 <poo_hoi@hotmail.com>
2019-04-19 10:43:59 -0400

    Update idler.js

--- idler.js
+++ idler.js
@@ -91,6 +91,7 @@ var research6 = [0, 90];
 document.getElementById("explorationzone").setAttribute("hidden", true);
 document.getElementById("coastEzone").setAttribute("hidden", true);
 document.getElementById("naviExploration").setAttribute("hidden", true);
+loadGame([woodraw,woodcut,stone,science,copperore,copperingot,tinore,tiningot,zincore,zincingot,manpower,clay,clayE,sand,sandE,glass,toolstation1[0],toolstation2[0],toolstation3[0],toolstation4[0],toolstation5[0],toolstation6[0],toolstation7[0],toolstation8[0],campsiteResearch1[0],research1[0],research2[0],research3[0],research4[0],research5[0],research6[0]]);
 function harvestWood() {
 	woodQueue++;
 	woodTime++
@@ -328,7 +329,7 @@ function clickNaviExploration() {
 				 }
 			}
 			//Geology
-loadGame([woodraw,woodcut,stone,science,copperore,copperingot,tinore,tiningot,zincore,zincingot,manpower,clay,clayE,sand,sandE,glass,toolstation1[0],toolstation2[0],toolstation3[0],toolstation4[0],toolstation5[0],toolstation6[0],toolstation7[0],toolstation8[0],campsiteResearch1[0],research1[0],research2[0],research3[0],research4[0],research5[0],research6[0]]);
+
 			
 setInterval(timer, tickspeed);
 
@@ -344,7 +345,7 @@ function loadGame(list) {
 		list[p] = localStorage[p];
 		console.log(localStorage[p]);
 		console.log(list[p]);
-		woodraw = localStorage[0];
+		//woodraw = localStorage[1];
 	}
 	console.log("Load Complete!");
 }
