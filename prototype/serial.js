5c82ba522935865554a527b353b04e27f4a787d3
hoi6789 <poo_hoi@hotmail.com>
2025-07-18 20:39:19 -0400

    uda

--- prototype/serial.js
+++ prototype/serial.js
@@ -7,7 +7,7 @@ var textEncoder = new TextEncoderStream();
 var readableStreamClosed;
 var writableStreamClosed;
 
-var enableDecoder = true;
+var enableDecoder = false;
 var enableEncoder = false;
 
 var table;
@@ -85,7 +85,29 @@ async function beginListening() {
 }
 
 async function sendMessage() { // BAD BAD BAD BAD BAD
+    // 9600 baud by default
+    // each message is 8 bytes (8 groups of 2 hex digits) long
+    // byte 1: header (fixed at 7c) 
+    // byte 2: length (fixed at 06 since 6 info bytes, 1 header, 1 footer)
+    // byte 3: address 1 (address of the receiver MCU)
+    // byte 4: command code (check command code table)
+    // byte 5: parameter (check command code table)
+    // byte 6: address 2 (address of the sender MCU)
+    // byte 7: checksum (XOR of bytes 2-6)
+    // byte 8: footer (fixed at cf)
+    // byte 6, 2 is basically like MCU0 -> 3? something like that. not that i know how it works
+    // command code has 2 nibbles: nibble 1 is the PX value (1-indexed), nibble 2 is 1 or 0 depending
+    // parameter is 8 bits, where every bit is the inverse of the set value except bit N which corresponds to the pin being set
+    // Px.7 = 0 -> 01111111; Px.6 = 0 -> 10111111
+    // Px.7 = 1 -> 10000000; Px.6 = 1 -> 01000000
+
+    // after receiving a command you should send a return message back
+    // command code is used as a return code which is just the command code + 80 
+    // parameter is always 0 in return messages
+    // remember to change checksum
+
     await writer.write(document.getElementById("input").value);
+    
 }
 
 async function flushBuffer() {
